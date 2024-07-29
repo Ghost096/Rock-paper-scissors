38b8cf2e6a3fec6fa40f28fbcd178bd2ceb738ca
Ghost096 <toxic656@gmail.com>
2024-07-29 10:21:39 +0200

    Added GUI

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 console.log("Hello world!")
 console.log(Math.random())
-let humanScore = 0
-let computerScore = 0
+const buttons = document.querySelector("#buttons");
+const buttons2 = document.querySelectorAll("button")
+const resultdoc = document.querySelector('#results');
+const scoreboard = document.querySelector('#score')
+let humanScoreCounter = 0
+let computerScoreCounter = 0
 
 function getHumanChoice(){
     const choiceHuman = prompt("Rock, paper or scissors?");
@@ -23,42 +27,91 @@ function getComputerChoice(){
 }
 
 
-
 function playRound(choiceHuman, choiceComputer){
+    const computerWin = "Computer won!";
+    const humanWin = "Human won!";
+    const gameTie = "It is tied!";
+    let result = "";
+
+    let scoreArray = {
+        roundResult: result,
+        computerScore: computerScoreCounter,
+        humanScore: humanScoreCounter,
+    }
+
     if (choiceComputer == "scissors" && choiceHuman=="paper"){
-        console.log("Computer won!");
-        computerScore +=1;
+        console.log(computerWin);
+        computerScoreCounter +=1;
+        scoreArray.result = "Computer won!";
     }
     else if (choiceComputer == "rock" && choiceHuman == "scissors"){
-        console.log("Computer won!");
-        computerScore +=1;
+        console.log(computerWin);
+        computerScoreCounter +=1;
+        scoreArray.result = "Computer won!";
     }
     else if (choiceComputer == "paper" && choiceHuman == "rock"){
-        console.log("Computer won!");
-        computerScore +=1;
+        console.log(computerWin);
+        computerScoreCounter +=1;
+        scoreArray.result = "Computer won!";
     }
     else if (choiceComputer == choiceHuman){
-        console.log("Its a tie!");
+        console.log(gameTie);
+        scoreArray.result = "It is tied!";
     }
     else if (choiceHuman == "scissors" && choiceComputer=="paper"){
-        console.log("Human won!")
-        humanScore +=1;
+        console.log(humanWin)
+        humanScoreCounter +=1;
+        scoreArray.result = "Human won!";
     }
     else if (choiceHuman == "rock" && choiceComputer == "scissors"){
-        console.log("Human won!")
-        humanScore +=1;
+        console.log(humanWin);
+        humanScoreCounter +=1;
+        scoreArray.result = "Human won!";
     }
     else if (choiceHuman == "paper" && choiceComputer == "rock"){
-        console.log("Human won!")
-        humanScore +=1;
+        console.log(humanWin)
+        humanScoreCounter +=1;
+        scoreArray.result = "Human won!";
     }
     else {
         console.log("Please enter rock, paper or scissors.")
     }
-    return computerScore, humanScore;
-}
+    if (humanScoreCounter == 5){
+        scoreArray.result = "Human is the winner!";
+        buttons2.forEach((button) => button.setAttribute('disabled', true));
 
-function playGame(){
+    } else if (computerScoreCounter == 5){
+        scoreArray.result = "Computer is winner!";
+        buttons2.forEach((button) => button.setAttribute('disabled', true));
+    }
+    return scoreArray;
+}
+function drawScore(scoreArray){
+    const result1 = document.createElement("p");
+    result1.textContent = scoreArray.result;
+    scoreboard.textContent = `Human: ${humanScoreCounter} Computer: ${computerScoreCounter}`;
+    resultdoc.appendChild(result1);
+}
+buttons.addEventListener('click', (event) =>{
+    let target = event.target;
+    if (humanScoreCounter < 5 && computerScoreCounter <5){
+        switch(target.id) {
+            case 'rock':
+                drawScore(playRound('rock', getComputerChoice()))
+                break;
+            case 'paper':
+                drawScore(playRound('paper', getComputerChoice()));
+                break;
+            case 'scissors':
+                drawScore(playRound('scissors', getComputerChoice()));
+                break;
+        }
+    }
+});
+
+
+
+/** function playGame(){
     let counter = 0;
     for (let i = 0; i < 5; i++) {
         playRound(getHumanChoice(), getComputerChoice());
@@ -74,4 +127,4 @@ function playGame(){
     }
 }
 
-playGame();
+playGame(); **/
